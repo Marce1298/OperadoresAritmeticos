775e683bda47832e1b9934ce7cfb786c7b765b5f
Marce1298 <marchebordon@gmail.com>
2020-09-17 18:10:02 -0300

    OperadoresAritmeticos

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace OperadoresAritmeticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(7 * 5);
+
+            Console.WriteLine(7 * 5 * 3);
+
+            Console.WriteLine(5 / 2);
+
+            Console.WriteLine(5.0 / 2.0);
+
+            Console.WriteLine(9 % 3);
+
+            Console.WriteLine(9 % 4);
+
+            int edad = 19;
+
+            edad++;
+
+            Console.WriteLine("Tienes una edad de " + edad + " años");
+
+            //Console.WriteLine($"Tienes una edad de {edad} años");
+
+            int edad2 = 19;
+
+            edad2 += 8;
+
+            Console.WriteLine("Tienes una edad de " + edad2 + "años");
+
+            int edad3 = 19;
+
+            edad3--;
+
+            Console.WriteLine("Tienes una edad de " + edad3 + " años");
+
+            int edad4 = 19;
+
+            edad4 -= 5;
+
+            Console.WriteLine("Tienes una edad de " + edad4 + "años");
+        }
+    }
+}
